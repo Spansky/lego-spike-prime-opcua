ad5a43eaa3a7b9b58c46d6a792cf656dc6d62506
leon <leon@codepals.com>
2024-06-06 19:17:32 +0200

    refactor: renamed variables

--- src/station.py
+++ src/station.py
@@ -3,16 +3,16 @@ import asyncio
 from buildhat import Matrix, ForceSensor, Motor
 
 # Connecting to the Lego Sensors
-matrix_d = Matrix('D')
-force_a = ForceSensor('A')
-motor_c = Motor('C')
+led_matrix = Matrix('D')
+force_sensor = ForceSensor('A')
+motor = Motor('C')
 
 # Store the initial force as it is set on OPCUA server startup
-force_value = force_a.get_force()
+force_value = force_sensor.get_force()
 
 # Return the force
 def get_force():
-    return force_a.get_force()
+    return force_sensor.get_force()
 
 # Indicate force with the Light Matrix
 def indicate_force():
@@ -28,7 +28,7 @@ def indicate_force():
 def start_sequence(color):
     for r in range(0,3):
         for c in range(0,3):
-            matrix_d.set_pixel((r,c), (color, 5))
+            led_matrix.set_pixel((r,c), (color, 5))
             time.sleep(0.05)
     move_motor(90, 20)
     move_motor(0, 20)
@@ -37,10 +37,10 @@ def start_sequence(color):
 def color_lightmatrix(colorcode):
     for r in range(0,3):
         for c in range(0,3):
-            matrix_d.set_pixel((r,c), (colorcode, 9))
+            led_matrix.set_pixel((r,c), (colorcode, 9))
 
 def move_motor(angle, speed): 
-    motor_c.run_to_position(angle, speed)
+    motor.run_to_position(angle, speed)
 
 # Infinite loop to continuously indicate force
 async def run_indicate_force():
