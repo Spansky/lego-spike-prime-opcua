3a61e55aed6dd38d35cd6c9f0c0554f66f9ba762
leon <leon@codepals.com>
2024-06-06 20:51:57 +0200

    feat: light indicates the force

--- src/station.py
+++ src/station.py
@@ -1,6 +1,6 @@
 import time
 import asyncio
-from buildhat import Matrix, ForceSensor, Motor
+from buildhat import Matrix, ForceSensor, Motor, Matrix
 
 # Connecting to the Lego Sensors
 led_matrix = Matrix('D')
@@ -39,6 +39,35 @@ def color_lightmatrix(colorcode):
         for c in range(0,3):
             led_matrix.set_pixel((r,c), (colorcode, 9))
 
+def indicate_force_level(percentage :int):
+    color_neutral :str = ""
+    color_meter :str = "green"
+    first_digit :int = percentage // 10
+    second_digit :int = percentage % 10
+
+    col :int = first_digit % 3
+    row :int = first_digit // 3 
+
+    for fill_up_row in range(0,row):
+        for col in range(0,3):
+            led_matrix.set_pixel((2-fill_up_row, col), (color_meter, 10))
+
+    for remaining_row in range(row, 3):
+        for col in range(0,3):
+            led_matrix.set_pixel((2-remaining_row, col), (color_neutral, 10))
+
+
+    #    for column in range(0,3):
+    #        print(f"row: {fill_up_row}, column: {column}")
+    #        led_matrix.set_pixel((fill_up_row,column),
+    #           (colorcode, 9))
+#    for fill_up_col in range(0, col):
+#        led_matrix.set_pixel((row + 1, fill_up_col), (colorcode, 9))
+#
+#    led_matrix.set_pixel((row + 1, col + 1),
+#                         (colorcode,
+#                         second_digit))
+
 def move_motor(angle, speed): 
     motor.run_to_position(angle, speed)
 
@@ -46,7 +75,7 @@ def move_motor(angle, speed):
 async def run_indicate_force():
     try:
         while True:
-            indicate_force()
+            indicate_force_level(get_force())
             await asyncio.sleep(0.05)  # Delay to control the loop speed
     except asyncio.CancelledError:
         print("Force indication stopped.")
