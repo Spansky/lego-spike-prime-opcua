d92be533b46a2108103043dbd997dce432b3d5ab
leon <leon@codepals.com>
2024-06-10 20:34:45 +0200

    feat: indicator function, but not applied yet

--- src/station.py
+++ src/station.py
@@ -7,22 +7,12 @@ led_matrix = Matrix('D')
 force_sensor = ForceSensor('A')
 motor = Motor('C')
 
-# Store the initial force as it is set on OPCUA server startup
-force_value = force_sensor.get_force()
+# parametrizable station params
+force_sweet_spot = 8 
 
 # Return the force
 def get_force():
     return force_sensor.get_force()
-
-# Indicate force with the Light Matrix
-def indicate_force():
-    force_val = get_force()
-    if force_val == 0:
-        color_lightmatrix(3)
-    elif 0 < force_val < 100:
-        color_lightmatrix(8)
-    elif force_val == 100:
-        color_lightmatrix(9)
 
 # A kind of hello world start sequence of actions when OPCUA server is started
 def start_sequence(color):
@@ -38,6 +28,12 @@ def color_lightmatrix(colorcode):
     for r in range(0,3):
         for c in range(0,3):
             led_matrix.set_pixel((r,c), (colorcode, 9))
+
+def indicate_sweet_spot(force_sweet_spot):
+    color_sweet_spot = "blue"
+    row = 2 - (force_sweet_spot // 3)
+    col = force_sweet_spot % 3
+    led_matrix.set_pixel((row,col),(color_sweet_spot, 9))
 
 def indicate_force_level(percentage :int):
     if not (0 <= percentage <= 100):
