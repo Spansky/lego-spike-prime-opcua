bef857b7d41710d70cbb26e3c947fa507dfa7cda
leon <leon@codepals.com>
2024-07-07 22:28:45 +0200

    feat: added descriptions to input params

--- src/opcuaserver.py
+++ src/opcuaserver.py
@@ -58,15 +58,26 @@ async def run():
     station_status = await opc_obj.add_variable(idx, "station_status",
                                                 station.get_station_status())
 
+    # Defining the input arguments for the @uamethod 'color_lightmatrix'
+    in_arg_colorcode = ua.Argument()
+    in_arg_colorcode.Name = "colorcode"
+    in_arg_colorcode.DataType = ua.NodeId(ua.ObjectIds.Int64)
+    in_arg_colorcode.ValueRank = -1
+    in_arg_colorcode.ArrayDimensions = []
+    in_arg_colorcode.Description = ua.LocalizedText(
+            "The color code 0-9 of the LED-Matrix"
+            )
+
     # Linking of the predefined @uamethod 'color_lightmatrix'
     await server.nodes.objects.add_method(
         ua.NodeId("LightMatrix", idx),
         ua.QualifiedName("LightMatrix", idx),
         color_lightmatrix,
-        [ua.VariantType.Int64],
-        [ua.VariantType.Int64],
+        [in_arg_colorcode],
+        [],
     )
 
+    # Defining the input arguments for the @uamethod 'move_motor'
     in_arg_engine_angle = ua.Argument()
     in_arg_engine_angle.Name = "angle"
     in_arg_engine_angle.DataType = ua.NodeId(ua.ObjectIds.Int64)
@@ -90,13 +101,21 @@ async def run():
         []
     )
 
+    # Defining the input arguments for the @uamethod 'set_force_upper_limit'
+    in_arg_force_limit = ua.Argument()
+    in_arg_force_limit.Name = "limit"
+    in_arg_force_limit.DataType = ua.NodeId(ua.ObjectIds.Int64)
+    in_arg_force_limit.ValueRank = -1
+    in_arg_force_limit.ArrayDimensions = []
+    in_arg_force_limit.Description = ua.LocalizedText("The upper limit 0 - 99 of the force")
+
     # Linking of the predefined @uamethod 'set_force_upper_limit'
     await server.nodes.objects.add_method(
         ua.NodeId("SetForceUpperLimit", idx),
         ua.QualifiedName("SetForceUpperLimit", idx),
         set_force_upper_limit,
-        [ua.VariantType.Int64],
-        [ua.VariantType.Int64]
+        [in_arg_force_limit],
+        []
     )
 
     # Linking of the predefined @uamethod 'reset_station_result'
